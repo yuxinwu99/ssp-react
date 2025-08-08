4af875aa6333a2477319f7359f6867763011f32c
Yuxin <yuxinwu1234@gmail.com>
2025-08-08 10:37:08 +0800

    updated orr form

--- src/routes/orr/form.tsx
+++ src/routes/orr/form.tsx
@@ -1,11 +1,40 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export const Route = createFileRoute("/orr/form")({
   component: RouteComponent,
 });
 
+type AppDetail = {
+  status: null;
+  systemType: null;
+  appCodeOwner: null;
+  appManager1: null;
+  appManager2: null;
+  hostingCountry: null;
+  usedInCountry: null;
+  lobt: null;
+  masCritical: null;
+  cat: null;
+  techMd: null;
+};
+const [appDetail, setAppDetail] = useState<
+  Record<keyof AppDetail, string | null>
+>({
+  status: null,
+  systemType: null,
+  appCodeOwner: null,
+  appManager1: null,
+  appManager2: null,
+  hostingCountry: null,
+  usedInCountry: null,
+  lobt: null,
+  masCritical: null,
+  cat: null,
+  techMd: null,
+});
 function RouteComponent() {
   return (
     <div className="m-5">
@@ -28,6 +57,14 @@ function RouteComponent() {
           <div className="flex flex-col items-center justify-center border border-gray-300 rounded-md p-3 min-h-[40px] focus-within:border-blue-500 focus-within:ring-1 focus-within:ring-blue-500 transition-colors">
             <span>Cloud App Code</span>
             <TextField required label="App Code" variant="outlined" />
+            <div>
+              {Object.entries(appDetail).map(([key, value]) => (
+                <div key={key}>
+                  <span>{key}</span>
+                  {value ? <span>{value}</span> : <span>N/A</span>}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </form>
